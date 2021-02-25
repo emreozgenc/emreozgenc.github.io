64cef3829ec527a57bb50f92aa3e741fc8c8df46
emreozgenc <emreozgenc@hotmail.com.tr>
2021-02-25 21:59:36 +0300

    update commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var typeIndex = 0;
+const typeText = 'Alanıma hoşgeldiniz...';
+
+const typeFunction = () => {
+
+    if(typeIndex < typeText.length) {
+        document.getElementById('title-2').innerHTML += typeText.charAt(typeIndex);
+        typeIndex++;
+
+        setTimeout(typeFunction, 75);
+    }
+
+}
+
+typeFunction();
